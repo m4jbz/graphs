a7dc13b87ce04f7bd09ddc71de532e499811e429
m4jbz <marcoajb05@proton.me>
2025-04-08 08:18:59 -0600

    xd

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module iti.informatica.graph {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
-    opens iti.informatica.graph to javafx.graphics, javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.desktop; // porque usas Toolkit de AWT
+    exports iti.informatica.graph;
 }
